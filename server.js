a6a80213098ad4d0b09e14aaaa450da36c716fff
derikbf <derikb@gmail.com>
2022-02-28 15:14:45 -0300

    Updated Server with server listen

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('port', port);
 const server = http.createServer(app);
 const router = express.Router();
 
-const route = route.get('/', (req, res, next) => {
+const route = router.get('/', (req, res, next) => {
   res.status(200).send({
     title: "Node Store API",
     version: "0.0.1"
@@ -19,3 +19,6 @@ const route = route.get('/', (req, res, next) => {
 });
 
 app.use('/', route);
+
+server.listen(port);
+console.log("API rodando na porta " + port);
