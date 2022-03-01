b89dd15c571e58bf27a8ea36c8186fcee85a42f7
derikbf <derikb@gmail.com>
2022-02-28 23:13:22 -0300

    Updated Product Controller and Repository with Async Await

--- src/controllers/product-controllers.js
+++ src/controllers/product-controllers.js
@@ -5,47 +5,51 @@ const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator')
 const repository = require('../repositories/product-repository')
 
-exports.get = (req, res, next) => {
-  repository
-    .get()
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-  });
+exports.get = async(req, res, next) => {
+  try {
+    const data = await repository.get();
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send({
+      message: 'Failed to process request!'
+    });
+  }
 }
 
-exports.getBySlug = (req, res, next) => {
-  repository
-    .getBySlug(req.params.slug)
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-  });
+exports.getBySlug = async (req, res, next) => {
+  try {
+    const data = await repository.getBySlug(req.params.slug);
+    res.status(200).send(data);
+  } catch (e) {
+      res.status(500).send({
+        message: 'Failed to process request!'
+    });
+  }
 }
 
-exports.getById = (req, res, next) => {
-  repository
-    .getById(req.params.id)
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-  });
+exports.getById = async (req, res, next) => {
+  try {
+    const data = await repository.getById(req.params.id);
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send({
+      message: 'Failed to process request!'
+    });
+  }
 }
 
-exports.getByTag = (req, res, next) => {
-  repository
-    .getByTag(req.params.tag)
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-  });
+exports.getByTag = async (req, res, next) => {
+  try {
+    const data = await repository.getByTag(req.params.tag);
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send({
+      message: 'Failed to process request!'
+    });
+  }
 }
 
-exports.post = (req, res, next) => {
+exports.post = async (req, res, next) => {
   let contract = new ValidationContract();
   contract.hasMinLen(req.body.title, 3, 'Min 3 caracters');
   contract.hasMinLen(req.body.slug, 3, 'Min 3 caracters');
@@ -56,43 +60,37 @@ exports.post = (req, res, next) => {
     return;
   }
   
-  repository
-    .create(req.body)
-    .then(x => {
-      res.status(201).send({ message: "Product created"});
-
-    }).catch(e => {
-      res.status(400).send({ message: "Failed Created Product", 
-      data: e
-    });
-  });
+  try {
+    await repository.create(req.body);
+    res.status(201).send({
+        message: "Product created"});
+    } catch (e) {
+      res.status(500).send({
+        message: 'Failed to process request!'
+      });
+    } 
 };
 
-exports.put = (req, res, next) => {
-  repository
-    .update(req.params.id, req.body)
-    .then(x => {
+exports.put = async (req, res, next) => {
+  try {
+    await repository.update(req.params.id, req.body);
+    res.status(200).send({
+        message: 'Product successfully updated'});
+  } catch (e) {
+    res.status(500).send({
+      message: 'Failed to process request!'
+    });
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    await repository.delete(req.body.id)
         res.status(200).send({
-          message: 'Product successfully updated'
-        });
-      }).catch(e => {
-        res.status(400).send({
-          messa: 'Failed to update the product!',
-          data: e
-      });
+          message: 'Product successfully removed!'});
+  } catch (e) {
+    res.status(500).send({
+      message: 'Failed to process request!'
     });
-};
-
-exports.delete = (req, res, next) => {
-  repository.delete(req.body.id)
-    .then(x => {
-      res.status(200).send({
-        message: 'Product successfully removed!'
-      });
-    }).catch(e => {
-      res.status(400).send({
-        messa: 'Failed to remove the product!',
-        data: e
-      });
-    });
+  }
 };
