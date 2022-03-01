447d9f28b1c5c3b8c6b2b7d6e503d865a70e8935
derikbf <derikb@gmail.com>
2022-03-01 13:15:46 -0300

    Inserted Refresh Token

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -71,3 +71,37 @@ exports.authenticate = async (req, res, next) => {
       });
     } 
 };
+
+exports.refreshToken = async (req, res, next) => {
+  try {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const data = await authService.decodeToken(token);
+
+    const customer = await repository.getById(data);
+
+    if (!customer) {
+      res.status(404).send({ 
+        message: 'Customer not found'
+      });
+      return;
+    }
+
+    const tokenData = await authService.generateToken({
+      id: customer.id,
+      email: customer.email,
+      name: customer.name
+    });
+
+    res.status(201).send({
+        token: token,
+        data: { 
+          email: customer.email,
+          name: customer.name
+        }
+      });
+    } catch (e) {
+      res.status(500).send({
+        message: 'Failed to process request!'
+      });
+    } 
+};
