4c398a9670e6e252b302ed7b5f436a4961c6709f
derikbf <derikb@gmail.com>
2022-03-01 10:19:20 -0300

    Send Email Welcome

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
- global.SALT_KEY = ''
- global.EMAIL_TMPL = '<strong>{0}</strong>';
+require('dotenv').config();
+global.SALT_KEY = `${process.env.SALT_KEY}`
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Node Store!';
 
- module.exports = {
-   connectionString: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@balta-io.52wjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-   sendgridkey: 'TBD',
-   ContainerConnectionString: 'TBD'
- }
+module.exports = {
+  connectionString: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@balta-io.52wjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  sendgridkey: `${process.env.SEND_KEY}`,
+  ContainerConnectionString: 'TBD'
+}
